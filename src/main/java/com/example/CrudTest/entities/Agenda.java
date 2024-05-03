05687bfa91af0bd8f377cb8b1179850d67dfef0d
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2024-05-03 15:47:38 -0600

    Initial commit <CON BASE DE DATOS H2 EN MEMORIA>

--- src/main/java/com/example/CrudTest/entities/Agenda.java
+++ src/main/java/com/example/CrudTest/entities/Agenda.java
@@ -26,10 +26,6 @@ public class Agenda {
     @NotEmpty(message = "No puede estar vacio")
     private String name;
 
-    @Min(value = 1, message = "el precio debe ser 1 o mayor")
-    @Max(value = 10000, message = "No hay productos en el almacen")
-    private Integer price;
-
     @Column(name ="created_at")
     @Temporal(TemporalType.DATE) //espesificar que es tipo fecha
     private Date createdAt;
